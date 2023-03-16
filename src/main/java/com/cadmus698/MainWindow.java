8263de01ec89815ac0e90f0f855a65923e8d6e38
Ani T <ananmay.t@gmail.com>
2023-03-15 21:38:21 -0700

    gradle new

--- src/main/java/com/cadmus698/MainWindow.java
+++ src/main/java/com/cadmus698/MainWindow.java
@@ -2,12 +2,26 @@ package com.cadmus698;
 
 import javax.swing.*;
 
-import com.formdev.flatlaf.*;
+import com.formdev.flatlaf.FlatDarkLaf;
 
-public class MainWindow {
+import java.awt.*;
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+public class MainWindow extends JFrame{
     private JList list1;
+    private JPanel window;
 
+    public MainWindow(){
+        setContentPane(window);
+    }
     public static void main(String[] args) {
-
+        GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0];
+        FlatDarkLaf.setup();
+        MainWindow gui = new MainWindow();
+        gui.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        gui.setTitle("Task Management");
+        gui.setVisible(true);
+        gui.pack();
     }
 }
