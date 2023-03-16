8fe9b8d5e0c0488bc28a8c1e98ff9f51c34c47d1
Ani T <ananmay.t@gmail.com>
2023-03-15 19:27:52 -0700

    gradle new

---
+++ src/main/java/com/cadmus698/MainWindow.java
@@ -0,0 +1,13 @@
+package com.cadmus698;
+
+import javax.swing.*;
+
+import com.formdev.flatlaf.*;
+
+public class MainWindow {
+    private JList list1;
+
+    public static void main(String[] args) {
+
+    }
+}
